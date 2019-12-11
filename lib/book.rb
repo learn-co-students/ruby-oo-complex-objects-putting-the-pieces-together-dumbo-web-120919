29d447a34bcc7eae1e10b1b7c4750c83dc6b5252
itslizkim <elizabeth.kim93@gmail.com>
2019-12-10 22:47:49 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
+
+
+    # def initialize(title,author,page_count,genre)
+    #     @title = title
+    #     @author = author
+    #     @page_count = page_count
+    #     @genre = genre
+    # end
+
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+end
 
 
